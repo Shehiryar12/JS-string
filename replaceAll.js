510dd97235834d79036a2cbe36312809ddcfc2b7
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-15 16:44:27 +0500

    stringindex learning

---
+++ replaceAll.js
@@ -0,0 +1,3 @@
+ const cadite="He is behave like a good person";
+ const onemorecadite=cadite.replaceAll('jik')
+ console.log(onemorecadite);
---
+++ stringindex.js
@@ -0,0 +1,4 @@
+ const games="need for speed";
+ console.log(games.indexOf("for"))
+ 
+ //it return -1 if string is not found
